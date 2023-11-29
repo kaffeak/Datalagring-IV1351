fc85a1ea0979a2065194299bbcf78bc9b3f6fa45
Alex <kaffe.ak@gmail.com>
2023-11-29 11:09:43 +0100

    adding script for getting each instructors amount of lesson / per month

---
+++ getInstructorslessons.sql
@@ -0,0 +1,9 @@
+SELECT public.payment.instructor_id, public.person.first_name, public.person.last_name, COUNT(public.lesson.id)
+FROM public.payment
+INNER JOIN public.lesson ON public.payment.id = public.lesson.payment_id
+INNER JOIN public.instructor ON public.payment.instructor_id = public.instructor.id
+INNER JOIN public.person ON public.instructor.person_ssn = public.person.ssn
+WHERE public.payment.month = 'nov'
+GROUP BY public.payment.instructor_id, public.person.first_name, public.person.last_name
+HAVING COUNT(public.lesson.id) > 0
+ORDER BY public.payment.instructor_id;
