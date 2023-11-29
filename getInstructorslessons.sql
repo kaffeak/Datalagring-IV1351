9861ad36d037de5068dce13f60d5d5f3943e1f04
Alex <kaffe.ak@gmail.com>
2023-11-29 11:45:08 +0100

    fixing error where date are inputted in the wrong order

--- getInstructorslessons.sql
+++ getInstructorslessons.sql
@@ -1,4 +1,4 @@
-SELECT public.payment.instructor_id, public.person.first_name, public.person.last_name, COUNT(public.lesson.id)
+SELECT public.payment.instructor_id, public.person.first_name, public.person.last_name, COUNT(public.lesson.id) AS number_of_lessons
 FROM public.payment
 INNER JOIN public.lesson ON public.payment.id = public.lesson.payment_id
 INNER JOIN public.instructor ON public.payment.instructor_id = public.instructor.id
